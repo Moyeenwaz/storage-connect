1d68109cf290bd6692a991aad297bdbd3527de89
Moyeenwaz <moyinoluwaziri@gmail.com>
2022-08-20 12:18:27 +0100

    res-nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,170 @@
-import React from "react";
-import { Flex, Icon, Text, HStack, Button } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Flex,
+  Icon,
+  Text,
+  HStack,
+  Button,
+  Link,
+  VStack,
+  IconButton,
+  Box,
+} from "@chakra-ui/react";
 import { Logo } from "../Icons/Icons";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = ({ color }) => {
+  const [isActive, setIsActive] = useState(false);
+  const icon = isActive ? FaTimes : FaBars;
+
   return (
-    <Flex as="nav" w="100%" h="92px" align="center" bg={color}>
-      <Flex justify="space-between" align="center" w="90%" m="auto">
-        <Flex justify="space-between" align="center">
-          <Icon as={Logo} />
-          <Text
-            ml="7px"
-            fontSize="20px"
-            as="h1"
+    <Box as="nav" position="relative" w="100%">
+      <Flex w="100%" h="92px" align="center" bg={color}>
+        <Flex justify="space-between" align="center" w="90%" m="auto">
+          <Flex justify="space-between" align="center">
+            <Icon as={Logo} />
+            <Text
+              ml="7px"
+              fontSize="20px"
+              as="h1"
+              color="white"
+              fontWeight={"bold"}
+            >
+              Storage Connect
+            </Text>
+          </Flex>
+
+          <IconButton
+            as={icon}
+            bg="transparent"
             color="white"
-            fontWeight={"bold"}
+            cursor="pointer"
+            onClick={() => setIsActive(!isActive)}
+            _hover={{
+              background: "none",
+            }}
+            _active={{
+              background: "none",
+            }}
+            display={["block", "block", "none", "none"]}
+            zIndex="999"
+          />
+
+          <HStack
+            spacing="40px"
+            fontSize="20px"
+            color="white"
+            display={["none", "none", "block", "block"]}
           >
-            Storage Connect
-          </Text>
+            <Link
+              to="#"
+              _hover={{
+                color: "#FFCE31",
+              }}
+            >
+              Active Listings
+            </Link>
+            <Link
+              to="#"
+              _hover={{
+                color: "#FFCE31",
+              }}
+            >
+              Sell Your Self Storage
+            </Link>
+            <Link
+              to="#"
+              _hover={{
+                color: "#FFCE31",
+              }}
+            >
+              Free Evaluation
+            </Link>
+            <Link
+              to="#"
+              _hover={{
+                color: "#FFCE31",
+              }}
+            >
+              Learn
+            </Link>
+            <Button
+              bg="#FFCE31"
+              color="#021850"
+              fontSize="18px"
+              fontWeight="medium"
+              py="8px"
+              px="18px"
+              borderRadius="10px"
+              w="110px"
+              h="36px"
+            >
+              Contact
+            </Button>
+          </HStack>
         </Flex>
-        <HStack spacing="40px" fontSize="20px" color="white">
-          <Text
-            cursor="pointer"
-            _hover={{
-              color: "#FFCE31",
-            }}
-          >
-            Active Listings
-          </Text>
-          <Text
-            cursor="pointer"
-            _hover={{
-              color: "#FFCE31",
-            }}
-          >
-            Sell Your Self Storage
-          </Text>
-          <Text
-            cursor="pointer"
-            _hover={{
-              color: "#FFCE31",
-            }}
-          >
-            Free Evaluation
-          </Text>
-          <Text
-            cursor="pointer"
-            _hover={{
-              color: "#FFCE31",
-            }}
-          >
-            Learn
-          </Text>
-          <Button
-            bg="#FFCE31"
-            color="#021850"
-            fontSize="18px"
-            fontWeight="medium"
-            pt="8px"
-            pb="8px"
-            pl="18px"
-            pr="18px"
-            borderRadius="10px"
-            w="110px"
-            h="36px"
-          >
-            Contact
-          </Button>
-        </HStack>
       </Flex>
-    </Flex>
+      <VStack
+        spacing="40px"
+        color="white"
+        position="fixed"
+        w="100%"
+        h="100%"
+        top="0"
+        pt="100px"
+        right="50%"
+        transform="translateX(50%)"
+        zIndex="99"
+        background="rgba(0, 0, 0, 0.7)"
+        display={`${isActive ? "flex" : "none"}`}
+      >
+        <Link
+          to="#"
+          _hover={{
+            color: "#FFCE31",
+          }}
+        >
+          Active Listings
+        </Link>
+        <Link
+          to="#"
+          _hover={{
+            color: "#FFCE31",
+          }}
+        >
+          Sell Your Self Storage
+        </Link>
+        <Link
+          to="#"
+          _hover={{
+            color: "#FFCE31",
+          }}
+        >
+          Free Evaluation
+        </Link>
+        <Link
+          to="#"
+          _hover={{
+            color: "#FFCE31",
+          }}
+        >
+          Learn
+        </Link>
+        <Button
+          bg="#FFCE31"
+          color="#021850"
+          fontSize="18px"
+          fontWeight="medium"
+          py="8px"
+          px="18px"
+          borderRadius="10px"
+          w="110px"
+          h="36px"
+        >
+          Contact
+        </Button>
+      </VStack>
+    </Box>
   );
 };
 
